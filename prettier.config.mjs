886f9bdc4e5e7f2cd525ca600eceb67cae583c53
Logan Stapleton <lojostapleton@gmail.com>
2025-06-02 22:25:01 -0400

    added shadcn

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+const config = {
+    printWidth: 100,
+    tabWidth: 4,
+    singleQuote: true,
+    bracketSameLine: true,
+    trailingComma: 'es5',
+
+    plugins: ['prettier-plugin-tailwindcss'],
+    tailwindAttributes: ['className'],
+};
+
+export default config;
