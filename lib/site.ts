752e02d7d1f667275ed489676e8469a8f1dc1d72
Logan Stapleton <55635465+stapletl@users.noreply.github.com>
2026-04-05 21:51:54 -0400

    Add React effect guidance to agent docs (#57)
    * Add React guidance to agent docs
    
    * Update site description
    
    * Adjust site description punctuation

--- lib/site.ts
+++ lib/site.ts
@@ -1,5 +1,5 @@
 export const SITE_NAME = 'Nihongo Cards';
-export const SITE_DESCRIPTION = 'Introduction to Japanese with an intelligent flashcard system';
+export const SITE_DESCRIPTION = 'Introduction to Japanese with flashcards and quizzes';
 export const SITE_AUTHOR = 'Logan Stapleton';
 export const SITE_HOMEPAGE_URL = 'https://nihongo-cards-two.vercel.app/';
 export const SITE_GITHUB_URL = 'https://github.com/stapletl/nihongo-cards';
